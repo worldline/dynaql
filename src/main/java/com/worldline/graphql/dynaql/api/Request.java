2720b7444e7bf3ce7b9401d244c02a0db740461c
JB <jb.roux@ymail.com>
2020-08-04 18:05:37 +0200

    Add licence to files

--- src/main/java/com/worldline/graphql/dynaql/api/Request.java
+++ src/main/java/com/worldline/graphql/dynaql/api/Request.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.worldline.graphql.dynaql.api;
 
 public interface Request {
